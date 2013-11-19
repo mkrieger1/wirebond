d555fdef70ae5bc31e668eade265bcdec876f2ed
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-11-19 14:45:26 +0100

    improved setup.py so the extension module is placed inside the package

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup, Extension
 from wirebond import __version__
 
-geom2d_module = Extension('geom2d', sources=['src/geom2d.c',
-                                             'src/geom2dmodule.c'])
+geom2d_ext = Extension('wirebond.geom2d',
+                       sources=['src/geom2d.c', 'src/geom2dmodule.c'])
 
 setup(name='wirebond',
       version=__version__,
       description='Tools for creating wire bond fanouts',
       author='Michael Krieger',
       packages=['wirebond'],
-      ext_modules = [geom2d_module]
+      ext_modules = [geom2d_ext]
      )
 
