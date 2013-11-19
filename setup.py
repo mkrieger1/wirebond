d6b22718101a7ad448c00682a95e7012143b5de3
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2013-11-19 14:34:00 +0100

    first working version of a python extension module

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from wirebond import __version__
+
+geom2d_module = Extension('geom2d', sources=['src/geom2d.c',
+                                             'src/geom2dmodule.c'])
 
 setup(name='wirebond',
       version=__version__,
       description='Tools for creating wire bond fanouts',
       author='Michael Krieger',
       packages=['wirebond'],
+      ext_modules = [geom2d_module]
      )
 
--- src/geom2dmodule.c
+++ src/geom2dmodule.c
@@ -3,15 +3,27 @@
 
 static PyObject* geom2d_add(PyObject *self, PyObject *args)
 {
-    Point *p;
+    Point p;
     Point q;
-    if (PyArg_ParseTuple(args, "ffff", &(p->x), &(p->y), &(q.x), &(q.y))) {
-        geom2d_point_add(p, q);
-    }
-    return Py_BuildValue("ff", &(p->x), &(p->y));
+    if (!PyArg_ParseTuple(args, "ffff", &(p.x), &(p.y), &(q.x), &(q.y)))
+        return NULL;
+    geom2d_point_add(&p, q);
+    return Py_BuildValue("ff", p.x, p.y);
 }
 
+//-------------------------------------------------------------------
+// Method table
+//-------------------------------------------------------------------
 static PyMethodDef Geom2dMethods[] = {
     {"add", geom2d_add, METH_VARARGS, "Add two points."},
     {NULL, NULL, 0, NULL} // sentinel
 };
+
+//-------------------------------------------------------------------
+// Initialization function
+//-------------------------------------------------------------------
+PyMODINIT_FUNC initgeom2d(void)
+{
+    (void) Py_InitModule("geom2d", Geom2dMethods);
+}
+
